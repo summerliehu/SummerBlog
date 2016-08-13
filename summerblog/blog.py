5444034f454b15ca4a8a8d9e62cb62e60023c537
summerliehu <14111510017@fudan.edu.cn>
2016-08-13 11:53:01 -0700

    bootstrap

--- summerblog/blog.py
+++ summerblog/blog.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
-	return '<h1>Hello, World!</h1>'
+	return render_template('index.html')
 
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name = name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('404.html'), 500
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	manager.run()
