a124a5a3052ac39c9d8250dd0a3401a72ac2b82c
summerliehu <14111510017@fudan.edu.cn>
2016-08-14 14:45:57 +0800

    flask-moment

--- summerblog/blog.py
+++ summerblog/blog.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
-
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'This is your secret_key'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
