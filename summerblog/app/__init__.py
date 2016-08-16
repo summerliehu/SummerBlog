940f48d7631ad89fddc92b8937143b1223638ec2
summerliehu <14111510017@fudan.edu.cn>
2016-08-17 02:00:18 +0800

    reconstructed

--- summerblog/app/__init__.py
+++ summerblog/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
@@ -10,75 +10,17 @@ moment = Moment()
 db = SQLAlchemy()
 mail = Mail()
 
+def create_app(config_name):
+	app = Flask(__name__)
+	app.config.from_object(config[config_name])
+	config[config_name].init_app(app)
 
+	bootstrap.init_app(app)
+	mail.init_app(app)
+	moment.init_app(app)
+	db.init_app(app)
 
-app = Flask(__name__)
+	from .main import main as main_buleprint
+	app.register_blueprint(main_buleprint)
 
-app.config['SECRET_KEY'] = 'This is your secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
-manager = Manager(app)
-
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-	form = NameForm()
-	if form.validate_on_submit():
-		user = User.query.filter_by(username=form.name.data).first()
-		if user is None:
-			user = User(username=form.name.data)
-			db.session.add(user)
-			session['known'] = False
-		else:
-			session['known'] = True
-		session['name'] = form.name.data
-		form.name.data = ''
-		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name=session.get('name'),\
-	 known=session.get('known', False))
-
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html', name = name)
-
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-	return render_template('404.html'), 500
-
-class NameForm(Form):
-	name = StringField('What is your name?', validators=[Required()])
-	submit = SubmitField('Submit')
-
-class Role(db.Model):
-	__tablename__ = 'roles'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), unique=True)
-	users = db.relationship('User', backref='role', lazy='dynamic')
-
-	def __repr__(self):
-		return '<Role %r>' % self.name
-
-class User(db.Model):
-	__tablename__ = 'users'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), unique=True, index=True)
-	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-	def __repr__(self):
-		return '<User %r>' % self.username
-
-def make_shell_context():
-	return dict(app=app,db=db,User=User,Role=Role)
-manager.add_command("shell", Shell(make_context=make_shell_context))
-
-
-if __name__ == '__main__':
-	manager.run()
-
+	return app
