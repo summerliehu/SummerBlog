d8b060736a228ab082c03b88e5ae0fc89e058b28
summerliehu <14111510017@fudan.edu.cn>
2016-08-18 01:13:31 +0800

    login

--- summerblog/app/__init__.py
+++ summerblog/app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.mail import Mail
-from flask.ext.moment import Moment
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config
+
 
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
 mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
 	app = Flask(__name__)
@@ -19,8 +24,11 @@ def create_app(config_name):
 	mail.init_app(app)
 	moment.init_app(app)
 	db.init_app(app)
+	login_manager.init_app(app)
 
 	from .main import main as main_buleprint
 	app.register_blueprint(main_buleprint)
+	from .auth import auth as auth_blueprint
+	app.register_blueprint(auth_blueprint)
 
 	return app
