f0679bb86a5dee3ea305a1c6fe30a8c76a8d3174
summerliehu <14111510017@fudan.edu.cn>
2016-08-20 00:56:35 +0800

    permissions

--- summerblog/app/models.py
+++ summerblog/app/models.py
@@ -1,7 +1,7 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymoususerMixin
 from flask import current_app
 from . import login_manager
 
@@ -9,6 +9,8 @@ class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
+	default = db.Column(db.Boolean, default = False, index = True)
+	permissions = db.Column(db.Integer)
 	users = db.relationship('User', backref='role', lazy='dynamic')
 
 	def __repr__(self):
@@ -21,6 +23,14 @@ class User(UserMixin, db.Model):
 	username = db.Column(db.String(64), unique=True, index=True)
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 	password_hash = db.Column(db.String(128))
+
+	def __init__(self, **kwargs):
+		super(User, self).__init__(**kwargs)
+		if self.role is None:
+			if self.email == current_app.config['FLASKY_ADMIN']:
+				self.role = Role.query.filter_by(permissions=0xff).first()
+			else:
+				self.role = Role.query.filter_by(default=True).first()
 
 	@property
 	def password():
@@ -36,7 +46,7 @@ class User(UserMixin, db.Model):
 	def __repr__(self):
 		return '<User %r>' % self.username
 
-	#confirm account and by token 
+	# confirm account and by token 
 	confirmed = db.Column(db.Boolean, default=False)
 
 	def generate_confirmation_token(self, expiration=3600):
@@ -54,8 +64,40 @@ class User(UserMixin, db.Model):
 		self.confirmed = True
 		db.session.add(self)
 		return True
+	# examine whether a user has following permissions
+	def can(self, permissions):
+		return self.role is not None and \
+			(self.role.permissions & permissions) == permissions
+	def is_administrator(self):
 
+class Permission():
+	FOLLOW = 0x01
+	COMMENT = 0x02
+	WRITE_ARTICLES = 0x04
+	MODERATE_COMMENTS = 0x08
+	ADMINISTER = 0x80
 
 @login_manager.user_loader
 def load_user(user_id):
 	return User.query.get(int(user_id))
+@staticmethod
+def insert_roles():
+	roles = {
+		'User':(Permission.FOLLOW |
+				Permission.COMMENT |
+				Permission.WRITE_ARTICLES, True)
+		'Moderator':(
+				Permission.FOLLOW |
+				Permission.COMMENT |
+				Permission.WRITE_ARTICLES |
+				Permission.MODERATE_COMMENTS, False)
+		'Administrator':(0xff, False)
+	}
+	for r in roles:
+		role = Role.query.filter_by(name=r).first()
+		if role is None:
+			role = Role(name=r)
+		role.permissions = roles[r][0]
+		role.default = roles[r][1]
+		db.session.add(role)
+	db.session.commit()
