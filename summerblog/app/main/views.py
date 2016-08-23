534c17e043c37f58cf843121c9aadf8b4d8a2e65
summerliehu <14111510017@fudan.edu.cn>
2016-08-23 22:41:41 +0800

    userpage

--- summerblog/app/main/views.py
+++ summerblog/app/main/views.py
@@ -5,3 +5,10 @@ from . import main
 @main.route('/')
 def index():
 	return render_template('index.html')
+
+@main.route('/user/<username>')
+def user(username):
+	user = User.query.filter_by(username=username).first()
+	if user is None:
+		abort(404)
+	return render_template('user.html', user=user)
