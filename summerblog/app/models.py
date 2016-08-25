d928646de2dd768d675f65e21bf05fbef9092056
summerliehu <14111510017@fudan.edu.cn>
2016-08-25 23:35:29 +0800

    markdown html

--- summerblog/app/models.py
+++ summerblog/app/models.py
@@ -6,6 +6,8 @@ from flask import current_app, request
 from . import login_manager
 from datetime import datetime
 import hashlib
+from markdown import markdown
+import bleach
 
 class Role(db.Model):
 	__tablename__ = 'roles'
@@ -167,6 +169,18 @@ class Post(db.Model):
 	body = db.Column(db.Text)
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+	body_html = db.Column(db.Text)
+
+	@staticmethod
+	def on_changed_body(target, value, oldvalue, initiator):
+		allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 
+						'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul', 
+						'h1', 'h2', 'h3', 'h4', 'p']
+		target.body_html = bleach.linkify(bleach.clean(
+			markdown(value, output_format='html'),
+			tags=allowed_tags, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 class Permission():
 	FOLLOW = 0x01
